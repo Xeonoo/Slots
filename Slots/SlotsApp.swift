d3768688ac7d22c8d28ee731ca58a9e15bd3b86d
Maksymilian <ts3joker@gmail.com>
2022-07-06 11:40:04 +0200

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Maksymilian Patoń on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
